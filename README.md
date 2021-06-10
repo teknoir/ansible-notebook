0bdad0a9bae3a08998a7450f515852bcc8ef0f2f
Anders Åslund <anders.aslund@teknoir.ai>
2021-06-10 13:31:57 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ansible-notebook
+# Teknoir Ansible Notebook Demo
+Notebooks runs in namespaces, the scope of devices to control is defined by the namespace. 
+
+## Limitations
+* Namespaces become groups, we do not support namespaces with dashes(-).
+  * Dashes(-) will be replaced with underscores(_)
+* The Connection Plugin automatically enable tunnels for devices but...
+  * does not tear them down after
+    
+## List devices
+```bash
+ansible -i inventory.py --list-hosts all
+```
+
+## Patch devices
+```bash
+ansible-playbook -v -i inventory.py playbook.yaml --limit my-device-01,my-device-02
+```
---
+++ inventory.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+import json
+import base64
+import random
+from kubernetes import client, config
+from kubernetes.config import ConfigException
+
+"""
+Teknoir custom dynamic inventory script for Ansible, in Python.
+"""
+
+
+class TeknoirInventory(object):
+
+    def __init__(self):
+        self.inventory = {}
+        self.read_cli_args()
+
+        # Called with `--list`.
+        if self.args.list:
+            self.inventory = self.teknoir_inventory()
+        # Called with `--host [hostname]`.
+        elif self.args.host:
+            # Not implemented, since we return _meta info `--list`.
+            self.inventory = self.teknoir_inventory()
+        # If no groups or vars are present, return empty inventory.
+        else:
+            self.inventory = self.empty_inventory()
+
+        print(json.dumps(self.inventory, indent=4, sort_keys=True));
+
+    def decode(self, s):
+        return base64.b64decode(s.encode('utf-8')).decode('utf-8')
+
+    def _start_reverse_tunnel(self, custom_api, namespace, name):
+        # display.v(f"Start reverse tunnel", host=self.inventory)
+        port = str(random.randint(1024, 64511))
+        device_patch = {
+            "spec": {
+                "keys": {
+                    "data": {
+                        "tunnel": base64.b64encode(port.encode('utf-8')).decode('utf-8')
+                    }
+                }
+            }
+        }
+        custom_api.patch_namespaced_custom_object("kubeflow.org",
+                                                  "v1beta1",
+                                                  namespace,
+                                                  "devices",
+                                                  name,
+                                                  device_patch)
+        self.tunnel_opened = True
+        return port
+    
+    def teknoir_inventory(self):
+        config.load_incluster_config()
+        custom_api = client.CustomObjectsApi()
+        devices = custom_api.list_namespaced_custom_object(group="kubeflow.org",
+                                                           version="v1",
+                                                           plural="devices",
+                                                           namespace="avangard-production")
+
+        inventory = {
+            '_meta': {
+                'hostvars': {}
+            }
+        }
+        ssh_port = 2200
+        for device in devices['items']:
+            ansible_group = device["metadata"]["namespace"].replace('-', '_')
+            path = f'inv/{ansible_group}/'
+            if ansible_group not in inventory:
+                inventory[ansible_group] = {
+                    'hosts': [],
+                    'vars': {}
+                }
+                os.makedirs(path, exist_ok=True)
+            private_key_file = f'{path}{device["metadata"]["name"]}.pem'
+            if not os.path.isfile(private_key_file):
+                with open(private_key_file, 'w') as outfile:
+                    outfile.write(self.decode(device['spec']['keys']['data']['rsa_private']))
+                os.chmod(private_key_file, 0o400)
+            
+            tunnel_port = self.decode(device['spec']['keys']['data']['tunnel'])
+            if not tunnel_port.isdigit():
+                tunnel_port = self._start_reverse_tunnel(custom_api, device["metadata"]["namespace"], device["metadata"]["name"])
+
+            hostname = f'{device["metadata"]["name"]}'
+            inventory[ansible_group]['hosts'].append(hostname)
+            inventory['_meta']['hostvars'][hostname] = {
+                'ansible_connection': 'teknoir',
+                'ansible_port': tunnel_port,
+                'ansible_host': 'localhost',
+                'ansible_user': self.decode(device['spec']['keys']['data']['username']),
+                'ansible_sudo_pass': self.decode(device['spec']['keys']['data']['userpassword']),
+                'ansible_become': 'yes',
+                'ansible_become_user': 'root',
+                'ansible_become_pass': self.decode(device['spec']['keys']['data']['userpassword']),
+                'ansible_ssh_private_key_file': private_key_file,
+                'ansible_python_interpreter': '/usr/bin/python3',
+                'ansible_ssh_retries': 20,
+                'ansible_kubectl_namespace': device["metadata"]["namespace"],
+                'ansible_teknoir_device': device['metadata']['name']
+            }
+            ssh_port = ssh_port + 1
+        return inventory
+
+    # Empty inventory for testing.
+    def empty_inventory(self):
+        return {'_meta': {'hostvars': {}}}
+
+    # Read the command line args passed to the script.
+    def read_cli_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--list', action='store_true')
+        parser.add_argument('--host', action='store')
+        self.args = parser.parse_args()
+
+
+# Get the inventory.
+TeknoirInventory()
