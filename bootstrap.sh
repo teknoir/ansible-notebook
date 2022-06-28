0b0dc848316f71c92e5809244d18f1d946d327b9
Anders Åslund <anders.aslund@teknoir.ai>
2022-06-28 13:30:56 +0200

    Fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set -o pipefail
+set -eo pipefail
 
+mkdir -p /etc/ansible
 cp ansible.cfg /etc/ansible/ansible.cfg
 
 cp inventory.py /etc/ansible/inventory.py
-chmod +x ~/.inventory.py
+chmod +x /etc/ansible/inventory.py
 
 mkdir -p /usr/share/ansible/plugins/modules
 cp -rf module_plugins/* /usr/share/ansible/plugins/modules/
